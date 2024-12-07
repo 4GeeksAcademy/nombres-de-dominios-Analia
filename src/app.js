c8c1956ff243255550b8664ec0b2276dc63ee33c
AnaliaMagallanes <167925588+AnaliaMagallanes@users.noreply.github.com>
2024-12-07 11:42:22 +0000

    dominios Analia

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let array1 = ["the", "our"];
+let array2 = ["great", "big"];
+let array3 = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let todasLasCombinaciones = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let elemento1 of array1) {
+  for (let elemento2 of array2) {
+    for (let elemento3 of array3) {
+      todasLasCombinaciones.push(elemento1 + elemento2 + elemento3 + ".com");
+    }
+  }
+}
+
+console.log(todasLasCombinaciones);
